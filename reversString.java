a3a8e0b1fa3225b9bf37db272a763769b8e09221
kamalPandey123 <152475406+kamalPandey123@users.noreply.github.com>
2024-03-07 18:42:33 +0530

    Update reversString.java

--- reversString.java
+++ reversString.java
@@ -3,18 +3,20 @@
 import java.util.*;
 
 public class reversString {
+//  recursive function   
     public static String revers_String(String s, String newString, int i) {
 
         if (i == 0) {
             newString = newString + s.charAt(i);
             return newString;
         }
-        newString = newString + s.charAt(i);
+        newString = newString + s.charAt(i);// character from index i (i =  string.length - 1) add to newString 
         return revers_String(s, newString, i - 1);
 
     }
 
     public static void main(String... args) {
+        //Declare object 
         Scanner sc = new Scanner(System.in);
 
         String s = sc.nextLine();
@@ -22,12 +24,3 @@ public class reversString {
 
     }
 }
-/*
- * if (index == 0) {
- * System.out.print(s.charAt(index));
- * return;
- * }
- * System.out.print(s.charAt(index));
- * index--;
- * recurString(s, index);
- */
